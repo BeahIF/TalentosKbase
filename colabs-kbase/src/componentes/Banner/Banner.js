3a1dc62b1f50a3f89b0ca044144a737fa720ae3d
BeahIF <beatrizdacosta1@Gmail.com>
2024-07-22 12:09:24 -0300

    iniciando front com react

---
+++ colabs-kbase/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+     return (
+        <header className='banner'>
+        <img src="/imagens/Marketing-Maria-20-1024x594.png" alt="O banner principal da página"/>
+
+        </header>
+    )
+}
+document.createElement('img')
+export default Banner;
