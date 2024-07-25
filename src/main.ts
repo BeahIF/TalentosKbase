13a9a280e8438721df9588209cbcca705a785234
BeahIF <beatrizdacosta1@Gmail.com>
2024-07-25 16:30:11 -0300

    adicionando cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Permite requisições apenas do frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permitir cookies (se necessário)
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +18,6 @@ async function bootstrap() {
     }),
   );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(3000);
+  await app.listen(8485);
 }
 bootstrap();
